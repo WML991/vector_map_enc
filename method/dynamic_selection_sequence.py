83f19303b38c2321adc82dfe49d944ab5e2f875c
王明亮 <95595595+WML991@users.noreply.github.com>
2025-12-29 22:03:19 +0800

    Refactor dynamic_selection function formatting

--- method/dynamic_selection_sequence.py
+++ method/dynamic_selection_sequence.py
@@ -1,7 +1,6 @@
 import numpy as np
 from math import floor
 
-#
 def dynamic_selection(X, Y, Z, W ,x_coords, y_coords):
 
     Qx = floor(np.mod((x_coords.max() - x_coords.min()), 4))
@@ -10,16 +9,14 @@ def dynamic_selection(X, Y, Z, W ,x_coords, y_coords):
     if Qy == Qx:
         Qy = np.mod(Qy + 1, 4)
 
-    # 创建序列映射字典
     index_map = {0: X, 1: Y, 2: Z, 3: W}
 
-    # 根据 Qx 和 Qy 选择置乱序列
     Q1 = index_map[Qx]
     Q2 = index_map[Qy]
 
-    # 获取剩余的两个序列
     remaining_indices = {0, 1, 2, 3} - {Qx, Qy}
     Q3 = index_map[remaining_indices.pop()]
     Q4 = index_map[remaining_indices.pop()]
 
     return Q1, Q2, Q3, Q4
+
